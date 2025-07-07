5d8b887477ba136f467b48bd55360d704b099903
alexandra-Al <yelisa_alvarez@soy.sena.edu.co>
2025-07-07 04:29:12 -0500

    titulo Coleccion de libros centrado

--- books-app/src/components/templates/MainLayout.vue
+++ books-app/src/components/templates/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen bg-gray-50 p-6">
-    <h1 class="text-3xl font-bold mb-6">Gestor de Libros</h1>
+    <h1 class="text-3xl font-bold color-text mb-6 text-center">Colección De Libros</h1>
     <slot />
   </div>
 </template>
